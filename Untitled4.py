3dcd136f4857e68564a59d0834023357a831b98b
jolin-code <jozzz2208@gmail.com>
2023-02-27 15:11:26 -0600

    Create Untitled4.py

---
+++ Untitled4.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[50]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[51]:
+
+
+# Define data
+
+
+# In[52]:
+
+
+labels = ['A', 'B', 'C', 'D']
+values = [10, 15, 20, 5]
+
+
+# In[53]:
+
+
+# Create bar chart
+
+
+# In[54]:
+
+
+plt.bar(labels, values)
+
+
+# In[55]:
+
+
+# Add labels and title
+
+
+# In[56]:
+
+
+plt.xlabel('Category')
+plt.ylabel('Value')
+plt.title('IS445 Practice Bar Chart')
+
+
+# In[57]:
+
+
+# Show the chart
+
+
+# In[58]:
+
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
